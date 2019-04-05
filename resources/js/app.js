b53908e3fab92cbfa397814c27539582dcd7257b
Nadzeya <naduva0505@gmail.com>
2019-04-06 01:26:05 +0200

    Added ECMAScript 5

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,20 @@
-const $goodCardTemplate = document.getElementById('goodCard');
-const $goods = document.getElementById('goods');
+// ES6: const $goodCardTemplate = document.getElementById('goodCard');
+var $goodCardTemplate = document.getElementById('goodCard');
+// ES6: const $goods = document.getElementById('goods');
+var $goods = document.getElementById('goods');
 
-const GOODS = [
+// ES: const GOODS = [
+var GOODS = [
     { title: 'red rose', price: 100, picture: 'resources/img/rose-red.jpg' },
     { title: 'green rose', price: 120, picture: 'resources/img/rose-green.jpg' },
     { title: 'yellow rose', price: 130, picture: 'resources/img/rose-yellow.jpg' },
     { title: 'pink rose', price: 140, picture: 'resources/img/rose-pink.jpg' }
 ];
 
-const renderGoodsItem = (title, price, picture) => {
-    let card = $goodCardTemplate.cloneNode(true);
+// ES6: const renderGoodsItem = (title, price, picture) => {
+function renderGoodsItem(title, price, picture) {
+    // ES6: let card = $goodCardTemplate.cloneNode(true);
+    var card = $goodCardTemplate.cloneNode(true);
     card.removeAttribute('id');
     
     card.querySelector('.goodImg').src = picture;
@@ -18,9 +23,12 @@ const renderGoodsItem = (title, price, picture) => {
     return card;
 }
 
-const renderGoodsList = (list) => {
-    list.forEach(item => {
-        let filledCard = renderGoodsItem(item.title, item.price, item.picture);
+// ES6: const renderGoodsList = (list) => {
+function renderGoodsList(list) {
+    // ES6: list.forEach(item => {  
+    list.forEach(function(item) {
+        // ES6: let filledCard = renderGoodsItem(item.title, item.price, item.picture);
+        var filledCard = renderGoodsItem(item.title, item.price, item.picture);
         filledCard.style.display = 'block';
         $goods.appendChild(filledCard);
     }); 
