67195181a15618052d5455ac754bb621e8ec1b9b
Nadzeya <naduva0505@gmail.com>
2019-04-19 23:41:16 +0200

    Refactored app.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
 import GoodsItem from './view/goodsItem.js';
-import GoodsList from  './view/goodsList.js';
-import Basket from './logics/basket.js';
 import BasketItem from './view/basketItem.js';
+import GoodsList from  './view/goodsList.js';
 import BasketList from './view/basketList.js';
-import GoodsItemsConverter from './logics/converters/goodsItemsConverter.js';
 import BasketWindow from './view/basketWindow.js';
+import Basket from './logics/basket.js';
+import GoodsItemsConverter from './logics/converters/goodsItemsConverter.js';
+
+const API_URI = '../../db.json';
 
 const $goods = document.getElementById('goods');
 const $basketButton = document.getElementById('basketButton');
@@ -44,16 +46,14 @@ function clickedOnAddButton(event) {
   })
 }
 
-function fetchAndDisplayGoods(url, convert, createUI){
-  fetch(url)
-    .then((response) => response.json())
+window.addEventListener('load', event => {
+  fetch(API_URI)
+    .then(response => response.json())
     .then(result => {
-      goodsItems = convert(result.goods);
-      createUI(goodsItems)
+      goodsItems = GoodsItemsConverter.convert(result.goods);
+      createUI(goodsItems);
     })
-}
-
-fetchAndDisplayGoods('../../db.json', GoodsItemsConverter.convert, createUI);
+});
 
 $basketButton.addEventListener('click', event => {
   let basketItems = basket.results();
